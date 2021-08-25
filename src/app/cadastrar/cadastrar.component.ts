5483b707e7691adc15010c8c6e221de0e9dda9a5
Garrido10 <fernando.garrido98@gmail.com>
2021-08-25 05:44:17 -0300

    Blog 10 e Faixa

---
+++ src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.validaNome()
+  }
+
+  validaNome() {
+    let nome = (<HTMLInputElement>document.querySelector('#nome')).value
+    if (nome?.length < 5){
+    
+    }else{
+    
+    }
+  }
+
+}
